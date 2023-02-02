8e2409290c5654039046bb02613e64c3d2e38925
samsonOdunola <odunolasamson@yahoo.com>
2023-02-02 22:37:57 +0100

    updates

--- back_end/server.js
+++ back_end/server.js
@@ -7,7 +7,7 @@ const emailRouter = require("./Routes/email.route");
 
 App.use(express.urlencoded({ extended: true, limit: "50mb" }));
 App.use(express.json({ limit: "50mb" }));
-App.use(cors());
+// App.use(cors());
 App.use("/contact-us", emailRouter);
 App.use(express.static("./build"));
 App.get("/*", (req, res) => {
