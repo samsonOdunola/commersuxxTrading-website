adcc8f675d4e256af4d1ee6dff84ee809bec1bed
samsonOdunola <odunolasamson@yahoo.com>
2023-02-02 21:30:47 +0100

    rearranged server and package.json

--- back_end/server.js
+++ back_end/server.js
@@ -5,9 +5,9 @@ require("dotenv").config();
 const port = process.env.PORT || 3000;
 const emailRouter = require("./Routes/email.route");
 
-App.use(cors());
 App.use(express.urlencoded({ extended: true, limit: "50mb" }));
 App.use(express.json({ limit: "50mb" }));
+App.use(cors());
 App.use("/contact-us", emailRouter);
 App.use(express.static("./build"));
 App.get("/*", (req, res) => {
