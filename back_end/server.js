73eeec404ff33568f1a82eab06f2c32912737853
samsonOdunola <odunolasamson@yahoo.com>
2023-02-02 23:15:50 +0100

    fix 11

--- back_end/server.js
+++ back_end/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const App = express();
-// const cors = require("cors");
+const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 const emailRouter = require("./Routes/email.route");
 
 App.use(express.urlencoded({ extended: true, limit: "50mb" }));
 App.use(express.json({ limit: "50mb" }));
-// App.use(cors());
+App.use(cors());
 App.use("/contact-us", emailRouter);
 App.use(express.static("./build"));
 App.get("/*", (req, res) => {
